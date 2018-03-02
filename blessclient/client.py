71d323bd3e2a58936e939e7d5bda79450613f8c1
Michael Sawyer <michael.sawyer@veeva.com>
2018-03-02 09:32:44 -0800

    Default to BLESS backend.

--- blessclient/client.py
+++ blessclient/client.py
@@ -858,12 +858,9 @@ def main():
                 if ca_backend.lower() == 'hashicorp-vault':
                     vault_bless(args.nocache, bless_config)
                     success = True
-                elif ca_backend.lower() == 'bless':
+                else:
                     bless(region, args.nocache, args.gui, args.host, bless_config)
                     success = True
-                else:
-                    sys.stderr.write('{0} is an invalid CA backend'.format(ca_backend))
-                    sys.exit(1)
                 break
             except (ClientError, LambdaInvocationException, ConnectionError,
                     EndpointConnectionError) as e:
